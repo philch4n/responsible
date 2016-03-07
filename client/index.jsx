bd74164e4c556cbdc730925b81686ceec7beac9e
Jonathan Krone <jkrone@vt.edu>
2016-03-07 17:21:49 -0600

    (feat) remove index.jsx and fix syntax errors on index.hmtl

--- client/index.jsx
+++
@@ -1,9 +0,0 @@
-import ReactDOM from 'react-dom';
-import { ProfileContainer } from './components/ProfileContainer';
-
-console.log('profileContainer:', ProfileContainer);
-
-ReactDOM.render(
-  <ProfileContainer />,
-  document.getElementById('reactContainer')
-);
