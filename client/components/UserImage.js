bc235bd15b36e2f646e85f53972af600d3a8e77e
Jonathan Krone <jkrone@vt.edu>
2016-03-07 13:24:16 -0600

    (feat) Write ProfileInfo list, item, and container

--- client/components/UserImage.js
+++ client/components/UserImage.js
@@ -1,10 +1,12 @@
 
 export function UserImage({ userImage, imageType }) {
 	return (
+		<div className="userImage">
 		{
 			imageType === 'profile' ?
 				<img src={ userImage /*  portrait size */ } /> :
 				<img src={ userImage /*  icon size  */ } />
 		}
+		</div>
 	);
 };
---
+++ client/containers/ProfileContainer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { ProfileInfoItem } from './components/ProfileInfoItem';
+import { UserImage } from './components/UserImage';
+import { ProfileInfoItemList } from './components/ProfileInfoItemList';
+
+class ProfileContainer extends React.Component {
+
+  constructor ({ someUrlFromProps, profileInfoItems }) {
+    this.someUrlFromProps = someUrlFromProps;
+    this.profileInfoItems = profileInfoItems;
+  }
+
+  render() {
+    <div className="ProfileContainer">
+      <UserImage userImage={ this.someUrlFromProps } imageType="profile" />
+      <ProfileInfoItemList profileInfoItems={ this.profileInfoItems } />
+    </div>
+  }
+
+}
