9c900bfd5fda48a282776cadeda219536373a4f4
Grant Redfearn <grantredfearn@gmail.com>
2016-03-08 10:59:30 -0600

    (feat) began work on implementing socket.io into chat

--- server/apis/chat-api.js
+++ server/apis/chat-api.js
@@ -11,4 +11,15 @@ Incorporate Socket.io
 Need POST
 Need GET
 */
+ChatAPI.get('/rides', function(req, res){
+  Ride.getRides()
+    .then(sendStatusAndData(res, 200))
+    .catch(sendStatusAndData(res, 500, 'Server error getting rides list')
+})
 
+
+io.sockets.on('connection', function(socket){
+  socket.on('send message', function(data){
+    io.sockets.emit('new message', data);
+  });
+});
--- server/apis/notification-api.js
+++
@@ -1,1 +0,0 @@
-notification-api.js
