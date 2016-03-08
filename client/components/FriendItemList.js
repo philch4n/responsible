9150e3f1ee467c986d7d2849e2fbcf27cd302512
Kimberly Kost <kimberlykost@Kimberlys-MacBook-Pro.local>
2016-03-08 12:00:32 -0600

    (feat) Add header for Friends List

--- client/components/FriendItemList.js
+++ client/components/FriendItemList.js
@@ -3,6 +3,7 @@ import {FriendItem} from './FriendItem';
 export function FriendItemList({ friends }) {
   return (
     <div className="friendList">
+    <h1>Your Friends!</h1>
       {
         friends.map(function (friend) {
           return <FriendItem {...friend} />;
