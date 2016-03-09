6431771f78f4983aef169e6ec5dbab38fb04ef0c
JosephPena <arcx354@gmail.com>
2016-03-09 16:55:38 -0600

    (fixed) truncate issue on user, established foreign key functionality

--- migrations/20160308170921_schema.js
+++ migrations/20160308170921_schema.js
@@ -3,18 +3,18 @@ exports.up = function (knex, Promise) {
 
 knex.schema.createTableIfNotExists('users', function (table) {
   table.increments('id').primary();
-  table.string('username').unique().notNullable();
+  table.string('username').notNullable().unique();
   table.string('password').notNullable();
   table.string('first_name').notNullable();
   table.string('last_name').notNullable();
-  table.string('street_address').unique().notNullable();
-  table.string('city').notNullable;
-  table.string('state').notNullable;
-  table.integer('zipcode').notNullable;
-  table.integer('phone_number').unique().notNullable;
-  table.string('email').unique().notNullable();
+  table.string('street_address').notNullable();
+  table.string('city').notNullable();
+  table.string('state').notNullable();
+  table.integer('zipcode').notNullable();
+  table.string('phone_number').notNullable();
+  table.string('email').notNullable().unique();
   table.string('emergency_contact').notNullable();
-  table.string('avatar').unique().notNullable();
+  table.string('avatar').notNullable();
   table.timestamps();
 }),
 
@@ -22,12 +22,15 @@ knex.schema.createTableIfNotExists('friend', function (table) {
   table.increments('id').primary();
   table.integer('user').references('id').inTable('users').notNullable();
   table.integer('friend').references('id').inTable('users').notNullable();
+
+  // table.integer('user').inTable('users').references('id').notNullable();
+  // table.integer('friend').inTable('users').references('id').notNullable();
 }),
 
 knex.schema.createTableIfNotExists('ride', function (table) {
   table.increments('id').primary();
-  table.integer('id_ride').notNullable();
-  table.integer('id_user').notNullable();
+  table.integer('user').references('id').inTable('users').notNullable();
+  table.integer('friend').references('id').inTable('users').notNullable();
   table.string('location').notNullable();
   table.timestamps();
 }),
@@ -41,9 +44,9 @@ knex.schema.createTableIfNotExists('car', function (table) {
 
 knex.schema.createTableIfNotExists('chats', function (table) {
   table.increments('id').primary();
-  table.string('make').notNullable();
-  table.string('capacity').notNullable();
-  table.string('color').notNullable();
+  table.integer('user').references('id').inTable('users').notNullable();
+  table.integer('friend').references('id').inTable('users').notNullable();
+  table.timestamps();
 }),
 
 ]);
