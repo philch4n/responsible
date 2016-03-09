bc608fae70ffb208d96560cba84b728a588a1e08
Jonathan Krone <jkrone@vt.edu>
2016-03-09 11:17:26 -0600

    (feat) Edit view and user actionCreators because of new information. Definitely still not solid here

--- client/actionCreators/view.js
+++ client/actionCreators/view.js
@@ -1,15 +1,22 @@
 // action creators for changing view state
 
-export function drive(willDrive) {
+export function isDriver(willDrive) {
   return {
     type: 'SET_DRIVER',
     entry: willDrive,
   };
 };
 
-export function ride(willRide) {
+export function isRider(willRide) {
   return {
     type: 'SET_RIDER',
     entry: willRide,
+  };
+};
+
+export function displaySettings(bool) {
+  return {
+    type: 'DISPLAY_SETTINGS',
+    entry: bool,
   };
 };
