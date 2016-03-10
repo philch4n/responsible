3573f6afe07f0e91ef1dc864c264280adc2f8ad1
Jonathan Krone <jkrone@vt.edu>
2016-03-10 14:36:26 -0600

    (feat) Add user and ride reducers which handle their respective parts of the redux state tree.

--- client/reducers/user.js
+++ client/reducers/user.js
@@ -1,28 +1,14 @@
-import {List, Map} from 'immutable';
+import { Map } from 'immutable';
 
 export default function (state = Map(), action) {
-  console.log('reducing state:', state);
+  console.log('reducing user state:', state.toJS());
 
   switch (action.type) {
     case 'SET_DRIVER':
       return state.set('isDriver', action.entry);
     case 'SET_RIDER':
       return state.set('isRider', action.entry);
-    case 'DISPLAY_SETTINGS':
-      return state.set('displaySettings', action.entry);
-    case 'DISPLAY_MATCH_INFO':
-      return state.set('displayMatchInfo', action.entry);
-
-    // case 'REQUEST_PAIR':
-    //   return state.set('waitingForMatch', action.entry);
-    // case 'RECEIVE_PAIR':
-    //   return matchWithPair(state, action);
   };
 
   return state;
-};
-
-function matchWithPair(state, action) {
-  // isMatched turns to the action.entry (userId)
-  // waitingForMatch turns false
 };
