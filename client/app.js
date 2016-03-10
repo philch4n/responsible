59b577e93831b17ac2281297f95cbd886696a9fb
Kimberly Kost <kimberlykost@Kimberlys-MacBook-Pro.local>
2016-03-10 15:23:01 -0600

    blah

--- client/app.js
+++ client/app.js
@@ -32,6 +32,29 @@ const dummyState = {
               '16/025Pikachu_OS_anime_10.png/revision/20150102074354',
       fullName: 'PIKA',
     },
+    {
+      id: 8,
+      avatar: 'http://vignette1.wikia.nocookie.net/pokemon/images/b/b8/' +
+        '001Bulbasaur_Dream.png/revision/latest?cb=20140903033758',
+      fullName: 'Bulb',
+    },
+    {
+      id: 9,
+      avatar: 'http://petupon.com/wp-content/uploads/2014/05/259758xcitefun-cute-animals-0.jpg',
+      fullName: 'Ears',
+    },
+    {
+      id: 11,
+      avatar: 'https://s-media-cache-ak0.pinimg.com/236x/b6/83/9d/' +
+        'b6839df8dc84c73e6f641f8a05cf7347.jpg',
+      fullName: 'Bunneh',
+    },
+    {
+      id: 12,
+      avatar: 'http://www.funcage.com/blog/wp-content/uploads/2013/'
+       + '11/Cute-Animals-Saying-Hi-To-You-001.jpg',
+      fullName: 'Lazy Harp Seal',
+    },
   ],
   profileInfoItems: [
     { itemTitle: 'Austin' },
