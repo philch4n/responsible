3943dba357df93ef3894b7aaabd81f3d08997081
Jonathan Krone <jkrone@vt.edu>
2016-03-10 16:10:54 -0600

    (refactor) rename setting button click tracker

--- client/components/SettingIcon.js
+++ client/components/SettingIcon.js
@@ -5,14 +5,18 @@ require('../public/styles/normalize.css');
 function nullFn(e) { console.log('you clicked me ' + e.target.className); };
 
 export function SettingIcon({
-  onSettingButtonClick = nullFn,
+  onSettingsButtonClick = nullFn,
 }) {
   return (
-    <div className='SettingIcon two columns' onClick={onSettingButtonClick}>
-      <select className='SettingButton btn u-full-width'>
-        <option>Settings</option>
-        <option>Profile</option>
-      </select>
+
+    // Ah! Sorry, Kim! Let's talk about what to do with these conflicts tomorrow morning.
+    // <div className='SettingIcon two columns' onClick={onSettingButtonClick}>
+    //   <select className='SettingButton btn u-full-width'>
+    //     <option>Settings</option>
+    //     <option>Profile</option>
+    //   </select>
+    <div className='SettingIcon two columns' onClick={onSettingsButtonClick}>
+      <button className='SettingButton btn u-full-width' type='button'>Settings!</button>
     </div>
   );
 }
