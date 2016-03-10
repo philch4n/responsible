5c343fffda045499d929f1d25446d89129ac3899
Kimberly Kost <kimberlykost@Kimberlys-MacBook-Pro.local>
2016-03-10 15:24:22 -0600

    (fix) Take out testing infinite scroll

--- client/components/Splash.js
+++ client/components/Splash.js
@@ -1,28 +1,11 @@
 import { DriveButton } from './DriveButton';
 import { CatchARideButton } from './CatchARideButton';
-import { Infinite } from 'react-infinite';
 
 export function Splash() {
   return (
-    <div className='testing'>
-      <Infinite>
-        <div>Hello</div>
-        <div>Testing</div>
-        <div>Hi</div>
-        <div>Does it have to be div?</div>
-        <div>Hola</div>
-        <div>Panda</div>
-        <div>asdfk</div>
-        <div>Power</div>
-        <div>Ranger</div>
-      </Infinite>
+    <div className='splash'>
+      <DriveButton />
+      <CatchARideButton />
     </div>
-    );
+  );
 };
-
-  // return (
-  //   <div className='splash'>
-  //     <DriveButton />
-  //     <CatchARideButton />
-  //   </div>
-  // );
