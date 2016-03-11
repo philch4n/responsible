c6dfc28c61906913f2ba1414b4eabe6baaaa111f
Grant Redfearn <grantredfearn@gmail.com>
2016-03-10 19:43:41 -0600

    (feat) working with chat tests

--- server/models/chat.js
+++ server/models/chat.js
@@ -1,3 +1,20 @@
+require('../server-helpers');
+var db      = require('../../lib/db.js');
+const first = require('ramda').head;
+
+module.exports = Chat;
+
+Chat.getChat = function () {
+  return db.select('*').from('chat');
+};
+
+Chat.getChatById = function (chatId) {
+  return db.select('*').from('chat').where({ id: chatId })
+    .catch(reportError('error retrieving chat by chatId'))
+    .then(first);
+};
+
 /*
 getChat
 getChatById
+*/
