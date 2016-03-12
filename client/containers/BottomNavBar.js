b96e1d127ef24e464b88fca72522d10c68bb5381
Kimberly Kost <kimberlykost@Kimberlys-MacBook-Pro.local>
2016-03-12 10:22:51 -0600

    (cleanup) Take out extraneous require statements

--- client/containers/BottomNavBar.js
+++ client/containers/BottomNavBar.js
@@ -6,9 +6,6 @@ import { BottomNavBarButton } from '../components/BottomNavBarButton';
 import * as userAction from '../actionCreators/user';
 import * as rideAction from '../actionCreators/ride';
 
-require('../public/styles/normalize.css');
-require('../public/styles/skeleton.css');
-
 /* Jonathan: does this feel too clunky to you? I was debating passing down props and
 pulling out the objects we need in each container (like: props.ride). Not sure... */
 function BottomNavBar({ ride, onConfirmLocationButtonClick }) {
