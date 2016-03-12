05f79b8affc7be98afedcaed5a55973f28320d94
Kimberly Kost <kimberlykost@Kimberlys-MacBook-Pro.local>
2016-03-12 10:38:29 -0600

    (cleanup) Code cleanup! Refactor Splash container to function

--- client/components/DriverInfoItem.js
+++ client/components/DriverInfoItem.js
@@ -1,7 +1,6 @@
 import { UserImage } from './UserImage';
 
 export function DriverInfoItem(props) {
-  console.log('am i stopoid', props);
 
   /* An object with a friend object gets send along, will probably need to refactor */
   return (
