f54102f6c142f29f2c98b262339e9ce905215a80
Grant Redfearn <grantredfearn@gmail.com>
2016-03-14 15:01:00 -0500

    (fix) changed car table to have a foreign user tag

--- server/models/user.js
+++ server/models/user.js
@@ -5,6 +5,7 @@ const first = require('ramda').head;
 var User = {};
 module.exports = User;
 
+// get all users
 User.getUsers = function () {
   return db.select('*').from('users')
     .catch(reportError('error retrieving username by userId'))
@@ -13,18 +14,21 @@ User.getUsers = function () {
     });
 };
 
+// find a user by their user_id
 User.findUserById = function (userId) {
   return db.select('*').from('users').where({ user_id: userId })
     .catch(reportError('error retrieving username by userId'))
     .then(first);
 };
 
+// delete at user by their user_id
 User.deleteUser = function (userId) {
   return db('users').where({ user_id: userId }).del()
     .catch(reportError('error deleting user by id'))
     .then(user => console.log('user', user));
 };
 
+// create a brand new user
 User.createUser = function (attrs) {
   return db('users').insert(attrs, ['user_id', 'username', 'first_name'])
     .catch(reportError('error creating user into db'))
