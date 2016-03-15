2df8a2a19c5e60089e05cc9254027ed2225d321a
Grant Redfearn <grantredfearn@gmail.com>
2016-03-15 11:14:18 -0500

    (feat) changed schema to accomidate rider and driver. rider and driver models

--- server/apis/rides-api.js
+++ server/apis/rides-api.js
@@ -20,6 +20,7 @@ RideAPI.post('/', function (req, res) {
     .catch(sendStatusAndError(res, 500, ('error creating user')));
 });
 
+// Delete rider by id
 RideAPI.delete('/:id', function (req, res) {
   var id = req.params.id;
   Ride.deleteRide(id)
@@ -27,3 +28,39 @@ RideAPI.delete('/:id', function (req, res) {
     .catch(sendStatusAndError(res, 500));
 });
 
+// Get All Riders
+RideAPI.get('/rider', function (req, res) {
+  Ride.getRiders()
+    .then(sendStatusAndData(res, 201))
+    .catch(sendStatusAndError(res, 500, ('error creating rider')));
+});
+
+// Get Rider By Id
+RideAPI.get('/rider/:id', function (req, res) {
+  var id = req.params.id;
+  Ride.getRiderById(id)
+    .then(sendStatusAndData(res, 200))
+    .catch(sendStatusAndError(res, 500));
+});
+
+// Post Rider
+RideAPI.post('/rider', function (req, res) {
+  var attrs = req.body;
+  Ride.createRider(attrs)
+    .then(sendStatusAndData(res, 201))
+    .catch(sendStatusAndError(res, 500, 'error creating rider'));
+});
+
+// Get All Drivers
+RideAPI.get('/driver', function (req, res) {
+  Ride.getDrivers()
+    .then(sendStatusAndData(res, 201))
+    .catch(sendStatusAndError(res, 500, 'error retrieving drivers'));
+});
+
+// Get driver by id
+RideAPI.get('/driver/' + id)
+
+
+// Post Driver
+RideAPI.post('/driver')
--- server/models/rides.js
+++ server/models/rides.js
@@ -5,6 +5,7 @@ const first = require('ramda').head;
 var Ride = {};
 module.exports = Ride;
 
+// Get all rides
 Ride.getRides = function () {
   return db.select('*').from('rides')
     .catch(reportError('error getting all rides'))
@@ -13,15 +14,63 @@ Ride.getRides = function () {
     });
 };
 
+// Create A ride
 Ride.createRide = function (attrs) {
-  return db('rides').insert(attrs, ['ride_id', 'foreign_driver', 'foreign_rider'])
+  return db('rides').insert(attrs, ['ride_id', 'ride_driver', 'ride_rider'])
     .catch(reportError('error creating ride in db'))
     .then(ride => ride);
 };
 
+// Delete A Ride
 Ride.deleteRide = function (id) {
   return db('rides').where({ ride_id: id }).del()
     .catch(reportError('error deleting ride by id'))
     .then(ride => console.log('deleted ride with id ' + id));
+};
+
+// Get all riders ids. returns array
+Ride.getRidersIds = function () {
+  return db.select('*').from('rider')
+    .catch(reportError('error getting all riders'))
+    .then(function (riders) {
+      return riders.map(rider => rider.rider_id);
+    });
+};
+
+// Get a rider by ID
+Ride.getRiderById = function (userId) {
+  return db.select('*').from('rider').where({ foreign_rider: userId })
+    .catch(reportError('error retrieving rider by userId'))
+    .then(first);
+};
+
+// Create a rider
+Ride.createRider = function (attrs) {
+  return db('rider').insert(attrs, ['rider_id', 'foreign_rider', 'location'])
+    .catch(reportError('error creating ride in db'))
+    .then(rider => rider);
+};
+
+// Get all drivers, returns array of their ids
+Ride.getDrivers = function () {
+  return db.select('*').from('driver')
+    .catch(reportError('error getting all drivers'))
+    .then(function (drivers) {
+      return drivers.map(driver => driver.driver_id);
+    });
+};
+
+// Get driver by ID
+Ride.getDriverById = function (userId) {
+  return db.select('*').from('driver').where({ foreign_driver: userId })
+    .catch(reportError('error retrieving rider by userId'))
+    .then(first);
+};
+
+// Create a new driver
+Ride.createDriver = function (attrs) {
+  return db('driver').insert(attrs, ['driver_id', 'foreign_driver', 'location'])
+    .catch(reportError('error creating driver in db'))
+    .then(driver => driver);
 };
 
