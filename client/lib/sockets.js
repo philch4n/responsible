669ca2a68937396858f095e37f73247c9c6a9da7
Jonathan Krone <jkrone@vt.edu>
2016-03-15 15:16:01 -0500

    (feat) add socket listeners to sockets.js (wishful)

--- client/lib/sockets.js
+++ client/lib/sockets.js
@@ -6,6 +6,12 @@ export const socket = io.connect('http://localhost:1337');
 console.log('socket:', socket);
 socket.on('connection', function (socket) {
   console.log('client connected!');
+
+  socket.on('need_ride', needRide);
+  socket.on('confirm_ride', confirmRide);
+  socket.on('cancel_ride', cancelRide);
+  socket.on('send_message', sendMessage);
+  socket.on('receive_message', receiveMessage);
 });
 
 export const remoteActionMiddleware =
