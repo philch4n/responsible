fe5dfd3a5b7ffeb6d9a3a6c00ac8b7ef91461e4a
Kimberly Kost <kek2009@gmail.com>
2016-03-15 14:11:13 -0500

    (feat) Chat box has form

--- client/containers/Chat.js
+++ client/containers/Chat.js
@@ -4,11 +4,20 @@ import { push } from 'react-router-redux';
 import { DriverItem } from '../components/DriverItem';
 import { MessageItemList } from '../components/Chat/MessageItemList';
 
-export function Chat({ match, id, messages, }) {
+function nullFn(e) {
+  e.preventDefault();
+  var message = e.target.firstChild.value;
+};
+
+export function Chat({ match, id, messages, onSubmit=nullFn, }) {
   return (
     <div className='chatbox'>
       <DriverItem {...match} />
       <MessageItemList userID={id} messages={messages}/>
+      <form onSubmit={onSubmit}>
+        <textarea className="u-full-width" placeholder="I am at..." id="message"></textarea>
+        <input className="button-primary" type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
