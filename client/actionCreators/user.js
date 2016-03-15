5df654e6d220ab7a785653aec482e8abdfaae62c
Jonathan Krone <jkrone@vt.edu>
2016-03-15 10:58:47 -0500

    (feat) add some user info actions.

--- client/actionCreators/user.js
+++ client/actionCreators/user.js
@@ -1,14 +1,34 @@
+import fetch from 'isomorphic-fetch';
 
-export function setDriver(isDriver) {
-  return {
-    type: 'SET_DRIVER',
-    entry: isDriver,
+export function fetchUserInfo(uniqueParam) {
+  return (dispatch) => {
+    dispatch(requestUserInfo())
+
+    fetch(`/users/${uniqueparam}`)
+      .then((info) => dispatch(receiveUserInfo(info)))
+      .catch((error) => dispatch(requestUserInfoError(error)));
   };
 };
+
+function requestUserInfo() {
+  return { type: 'REQUEST_USER_INFO' };
+}
+
+function receiveUserInfo(info) {
+  return { type: 'RECEIVE_USER_INFO', entry: info };
+}
+
+function requestUserInfoError(error) {
+  return { type: 'REQUEST_USER_INFO_ERROR', entry: error };
+}
+
+export function setDriver(isDriver) {
+  return { type: 'SET_DRIVER', entry: isDriver };
+}
 
 export function setRider(isRider) {
   return {
     type: 'SET_RIDER',
     entry: isRider,
   };
-};
+}
