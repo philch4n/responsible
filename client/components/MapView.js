93bb685d5878bc24d9593da3d0cf08d51b55c9cd
JosephPena <arcx354@gmail.com>
2016-03-15 17:16:16 -0500

    (feat) google maps updated

--- client/components/MapView.js
+++ client/components/MapView.js
@@ -3,16 +3,10 @@ import { GoogleMapLoader, GoogleMap } from 'react-google-maps';
 
 export function MapView() {
   return (
-  	<div className='map'>
-      <GoogleMapLoader
-        containerElement={ <div style={{ height: '70%' }} /> }
-         googleMapElement={
-          <GoogleMap
-          defaultZoom={14} defaultCenter={{ lat: 30.2672, lng: -97.7431 }}></GoogleMap>
-        }
-      />
-      </div>
-	);
+  <div className='map'>
+    <GoogleMapLoader
+      containerElement={ <div style={{ height: '80%' }} /> }
+      googleMapElement={ <GoogleMap defaultZoom={14} defaultCenter={{ lat: 30.2672, lng: -97.7431 }}></GoogleMap> }/>
+  </div>
+  );
 };
-
-
