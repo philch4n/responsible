61162b6758afe1d44e0f2311c373d60cdd02fc81
JosephPena <arcx354@gmail.com>
2016-03-15 19:00:57 -0500

    Update Main.js

--- client/containers/Main.js
+++ client/containers/Main.js
@@ -1,4 +1,4 @@
- import { connect } from 'react-redux';
+import { connect } from 'react-redux';
 
 import { TopNavBarContainer } from './TopNavBar';
 import { BottomNavBarContainer } from './BottomNavBar';
