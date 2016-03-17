1c7b48791d042a299e48e3478515cd3cdf6fb2bf
Grant Redfearn <grantredfearn@gmail.com>
2016-03-16 19:29:11 -0500

    (feat) began writing tests for friend riders and drivers

---
+++ test/server/test_models/friends_test.js
@@ -0,0 +1,30 @@
+require('../../test-helper');
+const db = require('../../../lib/db');
+const request = require('supertest');
+const routes = require(__server + '/index.js');
+const dbCleaner = require('knex-cleaner');
+const Friend = require(__models + '/friends');
+const Seed = require('../../lib/seed_test');
+
+var SeedObj = null;
+
+describe('Friends Models', function () {
+
+  beforeEach_(function * () {
+    yield Seed.cleaner();
+    SeedObj = yield* Seed.runner();
+
+  });
+
+  it_('Should get drivers who are friends', function * () {
+    var drivers = yield Friend.getFriendDrivers(SeedObj.user1Id[0].user_id);
+    expect(drivers).to.have.length(1);
+    expect(drivers).to.be.an.instanceOf(Array);
+  });
+
+  it_('Should get riders who are friends', function * () {
+    var riders = yield Friend.getFriendRiders(SeedObj.user3Id[0].user_id);
+    expect(riders).to.have.length(1);
+    expect(riders).to.be.an.instanceOf(Array);
+  });
+});
