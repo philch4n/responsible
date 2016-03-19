9667de49ee46af83f88be619ef32a378ac103c17
Kimberly Kost <kek2009@gmail.com>
2016-03-19 12:24:26 -0500

    (feat) Form for adding a user, also is able to take form submission

--- client/containers/Friends.js
+++ client/containers/Friends.js
@@ -1,14 +1,19 @@
 import { connect } from 'react-redux';
+import { curry } from 'ramda';
 
 import { FriendItemList } from '../components/TopNavBar/Friends/FriendItemList';
 
 function nullFn(e) { console.log('you clicked me ' + e.target.className); };
 
-function List({ friends }) {
-  console.log('friends', friends, FriendItemList);
+function List({ friends, addFriend, user_id }) {
+  console.log('friends', friends, FriendItemList, user_id);
   return (
     <div className="friendList">
       <FriendItemList friends={friends}/>
+      <form onSubmit={addFriend(user_id)}>
+        <input className="friendText ten columns" defaultValue='' id="message"></input>
+        <input className="friendSubmit button" type="submit" />
+      </form>
     </div>
   );
 };
@@ -17,20 +22,22 @@ const mapStateToProps = function (state) {
   return state.get('user').toJS();
 };
 
-// const mapDispatchToProps = function (dispatch) {
-//   return {
-//     onProfileButtonClick() {
-//       dispatch(push('/profile'));
-//     },
+const mapDispatchToProps = function (dispatch) {
+  return {
+    addFriend: curry(function (user_id, e) {
+      e.preventDefault();
+      let friendObject = {
+        userID: user_id,
+        text: e.target.firstChild.value,
+      };
+      e.target.firstChild.value = '';
 
-//     onFriendButtonClick() {
-//       dispatch(push('/friends'));
-//     },
-//   };
-// };
+      // dispatch(userAction.addFriend(friendObject));
+    }),
+  };
+};
 
 export const FriendList = connect(
-  mapStateToProps
-
-  // mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(List);
