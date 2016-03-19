ac7befc296a1903cb7d03504a0c1595e92889049
Grant Redfearn <grantredfearn@gmail.com>
2016-03-19 15:51:15 -0500

    (feat) working on adding driver. function calling automatically

--- client/components/Splash/DriveButton.js
+++ client/components/Splash/DriveButton.js
@@ -2,10 +2,10 @@
 function nullFn(e) { console.log('you clicked me ' + e.target.className); };
 
 export function DriveButton({
-  onDriveButtonClick = nullFn,
+  onDriveClick = nullFn,
 }) {
   return (
-    <div className='DriveDiv' onClick={onDriveButtonClick}>
+    <div className='DriveDiv' onClick={onDriveClick}>
       <button className='DriveButton btn u-full-width' type='button'>Drive!</button>
     </div>
   );
