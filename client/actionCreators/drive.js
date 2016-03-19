cfc94ef262af17d60e82222c009f2b5b83dfb9dc
Grant Redfearn <grantredfearn@gmail.com>
2016-03-19 14:01:16 -0500

    (feat) wokring on drive action creator

--- client/actionCreators/drive.js
+++ client/actionCreators/drive.js
@@ -1,4 +1,9 @@
 import fetch from 'isomorphic-fetch';
 import { headers, json, checkStatus } from '../lib/fetchHelpers';
 
-export function fetchDriver(state = List())
+export function fetchDriver(userId, location) {
+  return (dispatch) => {
+
+    fetch('/rides/drivers')
+  }
+}
