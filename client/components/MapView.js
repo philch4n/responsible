1ebb2e9dcfd531eb60bb03604ebc8bd28520396c
JosephPena <arcx354@gmail.com>
2016-03-19 10:45:37 -0500

    merged resolved

--- client/components/MapView.js
+++ client/components/MapView.js
@@ -1,45 +1,157 @@
 import React from 'react';
-import { GoogleMapLoader, GoogleMap } from 'react-google-maps';
+import { GoogleMapLoader, GoogleMap , Marker, DirectionsRenderer } from 'react-google-maps';
+import { connect } from 'react-redux'
 
-export function MapView(props) {
-
-  function errorError() {
-    alert("Location can't be found");
-  }
-
-  if (navigator.geolocation) {
 
     //clearWatch()    watchPosition()    getCurrentPosition()
-    navigator.geolocation.getCurrentPosition(GoogleMap, errorError);
-  } else {
-    alert('Your browser does not support Geolocation.');
-  };
+    // navigator.geolocation.getCurrentPosition(GoogleMap, errorError);
 
-  var austin = {
-    lat: 30.2672,
-    lng: -97.7431,
-  };
 
-  return (
-<<<<<<< b7053ad7893432082735b56a39910a5040fb9619
+const DirectionsService = new google.maps.DirectionsService();
+    DirectionsService.route({
+    origin: {lat:41.8507300, lng:-87.6512600},
+    destination: {lat:41.8525800, lng:-87.6514100},
+      travelMode: google.maps.TravelMode.DRIVING,
+    }, (result, status) => {
+      onDirectionsResult(result);
+      console.log('these are results', result)
+      console.log('these are status', status)
+      }
+    )
+
+
+//  return (
+//    <div className='map'>
+//       <GoogleMapLoader
+//       containerElement={ <div style={{ height: '70%' }} /> }
+//       googleMapElement={ <GoogleMap defaultZoom={14} defaultCenter={ location } >
+//       {directions ? <DirectionsRenderer directions={directions} /> : null}
+//       </GoogleMap> }
+//       />
+//     </div>
+//  );
+// };
+
+
+
+
+
+
+function Map ({ match, location, onMapView, directions }) {
+  return match ? 
+  (
     <div className='map'>
       <GoogleMapLoader
+      ref={(map) => console.log('map data1', map)}
       containerElement={ <div style={{ height: '70%' }} /> }
-      googleMapElement={
-        <GoogleMap
-          defaultZoom={14}
-          defaultCenter={{ lat: austin.lat, lng: austin.lng }}
-        />
-      }
+      googleMapElement={ <GoogleMap defaultZoom={14} defaultCenter={ location } >
+      {directions ? <DirectionsRenderer directions={directions} /> : null}
+      </GoogleMap> }
       />
     </div>
-	);
-=======
-  <div className='map'>
-    <GoogleMapLoader
-      containerElement={ <div style={{ height: '90%' }} /> }
-      googleMapElement={ <GoogleMap defaultZoom={14} defaultCenter={{ lat: 30.2672, lng: -97.7431 }}></GoogleMap> }/>
-  </div>
+  )
+  :
+  (
+    <div className='map'>
+      <GoogleMapLoader
+      ref={(map) => console.log('map data1', map)}
+      containerElement={ <div style={{ height: '70%' }} /> }
+      googleMapElement={ <GoogleMap defaultZoom={14} defaultCenter={ location } >
+      </GoogleMap> }
+      />
+    </div>
   );
->>>>>>> (feat) more maps updates
 };
+
+const mapDispatchToProps = function (dispatch) {
+  return {
+    onMapView() {
+      const DirectionsService = new google.maps.DirectionsService();
+
+      DirectionsService.route(
+        {
+          origin: { location },
+          destination: {lat:41.8525800, lng:-30.6514100},
+          travelMode: google.maps.TravelMode.DRIVING
+        },
+        function(result, status) {
+          // dispatch(result);
+          console.log('these are results', result);
+          console.log('these are status', status);
+        }
+      );
+    },
+  };
+};
+
+// const DirectionsService = new google.maps.DirectionsService();
+//     DirectionsService.route(
+//     {
+//     origin: { location },
+//     destination: {lat:41.8525800, lng:-87.6514100},
+//       travelMode: google.maps.TravelMode.DRIVING,
+//     }, 
+//     function (result, status) {
+//       console.log('these are results', result)
+//       console.log('these are status', status)
+//     }
+//     )
+
+
+export const MapView = connect(
+  null,
+  mapDispatchToProps
+)(Map);
+
+
+
+
+// function Github({ onGithubClick = nullFn, }) {
+//   return (
+//     <div className='GitHubButton' onClick={onGithubClick}>
+//       <button className='GithubButton btn'>Github</button>
+//     </div>
+//   );
+// }
+
+// const mapDispatchToProps = function (dispatch) {
+//   return {
+//     onGithubClick() {
+//       var user;
+//       OAuth.popup('github', { cache: true })
+//         .done(function (githubToken) {
+//           githubToken.me()
+//             .done(function (me) {
+//               console.log('found myself, finally:', me);
+
+//               user = {
+//                 username: me.alias,
+//                 first_name: me.name.split(' ')[0],
+//                 last_name: me.name.split(' ')[1],
+//                 avatar: me.avatar,
+//                 verifyBy: me.id,
+//                 token: githubToken.access_token,
+//               };
+
+//               dispatch(userAction.fetchUserInfo(user));
+//             });
+//         })
+//         .fail(function (err) {
+//           console.log('error', err);
+//         });
+//     },
+//   };
+// };
+
+// export const GithubButton = connect(
+//   null,
+//   mapDispatchToProps
+// )(Github);
+
+
+
+
+
+
+
+
