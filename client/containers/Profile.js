2359d4097b1be86ddacb83fcf01251eebab284c2
Jonathan Krone <jkrone@vt.edu>
2016-03-19 20:49:55 -0500

    (feat) gosh, I can't even find the value of an input field! Will finish tomorrow

--- client/containers/Profile.js
+++ client/containers/Profile.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { curry } from 'ramda';
 
 import { FriendItemList } from '../components/TopNavBar/Friends/FriendItemList';
 import { UserImage } from '../components/UserImage';
 import { ProfileItemList } from '../components/Profile/ProfileItemList';
 import * as userAction from '../actionCreators/user';
 
-function Profile({ friends, profile, onFriendClick, onAddressEdit }) {
+function Profile({ user_id, friends, profile, onFriendClick, onAddressEdit }) {
   // let profileItems = [
   //   { title: 'Name', desc: profile.fullName },
   //   { title: 'Home Address', desc: profile.address },
@@ -27,7 +28,7 @@ function Profile({ friends, profile, onFriendClick, onAddressEdit }) {
         <input className="profileItem"
           type="test"
           placeholder={profile.address}
-          onClick={onAddressEdit} />
+          onClick={onAddressEdit(user_id)} />
       </div>
     </div>
   );
@@ -40,14 +41,16 @@ const mapStateToProps = function (state) {
 
 const mapDispatchToProps = function (dispatch) {
   return {
-    onAddressEdit(e) {
+    onAddressEdit: curry((user_id, e) => {
+      let newAddress = e.target.firstChild.value;
       e.preventDefault();
-      console.log('changing address to:', e.target.value);
-      dispatch(userAction.changeAddress(e.target.value));
-    },
+      console.log('changing address to:', newAddress);
+      dispatch(userAction.changeAddress(user_id, newAddress));
+    }),
   };
 };
 
 export const ProfileContainer = connect(
-  mapStateToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(Profile);
