46360771505fedc6e33f04160fb00e40165619cb
JosephPena <arcx354@gmail.com>
2016-03-21 09:49:55 -0500

    (fix) removed unecessary code

--- client/components/MapView.js
+++ client/components/MapView.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import { GoogleMapLoader, GoogleMap, DirectionsRenderer } from 'react-google-maps';
+import { GoogleMapLoader, GoogleMap, Marker, DirectionsRenderer } from 'react-google-maps';
 import { connect } from 'react-redux';
 
 // overlayMapTypes
 // MapTypes
 function Map({ match, location, directions }) {
+  console.log('these are directions', directions);
   return match ?
   (
     <div className='map'>
       <GoogleMapLoader
+<<<<<<< a6a4e6c974378b27aa449620181d9d0bd81bf0b6
+=======
+        ref={(map) => console.log('MATCH!', map)}
+>>>>>>> (fix) removed unecessary code
         containerElement={ <div style={{ height: '70%' }} /> }
         googleMapElement={
           <GoogleMap defaultZoom={14} defaultCenter={ location } >
