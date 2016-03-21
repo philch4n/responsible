78987ae2748dab5817857cdf6b88cc94475b3c4f
Jonathan Krone <jkrone@vt.edu>
2016-03-21 16:52:30 -0500

    (fix) refactor intial state and chat and messaging to be cohesive

--- server/lib/ioConfig.js
+++ server/lib/ioConfig.js
@@ -21,6 +21,7 @@ IO.init = function (server) {
 
     // first listener!
     socket.on('new_message', function (data) {
+      console.log('broadcasting message to:', data.to);
       socket.broadcast.to(data.to).emit('new_message', data.entry);
     });
   });
