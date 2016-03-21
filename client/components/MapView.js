e5509a2cbe2d4d75b74c06296e1816d520ecddb1
JosephPena <arcx354@gmail.com>
2016-03-21 09:49:55 -0500

    (fix) removed unecessary code

--- client/components/MapView.js
+++ client/components/MapView.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import { GoogleMapLoader, GoogleMap, DirectionsRenderer } from 'react-google-maps';
+import { GoogleMapLoader, GoogleMap, Marker, DirectionsRenderer } from 'react-google-maps';
 import { connect } from 'react-redux';
 
 // overlayMapTypes
 // MapTypes
-function Map({ match, location, directions }) {
+function Map({ match, location, riders, directions }) {
+  console.log('riders:', riders)
   console.log('these are directions', directions);
   return match ?
   (
     <div className='map'>
       <GoogleMapLoader
-<<<<<<< a6a4e6c974378b27aa449620181d9d0bd81bf0b6
-=======
         ref={(map) => console.log('MATCH!', map)}
->>>>>>> (fix) removed unecessary code
-        containerElement={ <div style={{ height: '70%' }} /> }
+        containerElement={ <div style={ { height: '70%' } } /> }
         googleMapElement={
           <GoogleMap defaultZoom={14} defaultCenter={ location } >
             {
@@ -30,14 +28,30 @@ function Map({ match, location, directions }) {
   :
   (
     <div className='map'>
-      <GoogleMapLoader
-      ref={(map) => console.log('NO MATCH', map)}
-      containerElement={ <div style={{ height: '70%' }} /> }
-      googleMapElement={ <GoogleMap defaultZoom={14} defaultCenter={ location } >
-      </GoogleMap> }
-      />
+    <GoogleMapLoader
+       ref={(map) => console.log('OH YOU WALKIN!', map)}
+       containerElement={<div style={{ height: '100%' }} />}
+       googleMapElement={
+        <GoogleMap defaultZoom={14} defaultCenter={ location } >
+         {
+          riders.map((rider) => {
+            console.log('this is rider, ryder, rydur', rider);
+            let riderMarker = {};
+            riderMarker.position = rider.location;
+            riderMarker.defaultAnimation = 1;
+
+            return (
+              <Marker {...riderMarker} />
+              //properly iterates through riders in initialState but only maps 
+            );
+          })
+        };
+         </GoogleMap>
+       }
+     />
     </div>
   );
 };
 
 export const MapView = connect()(Map);
+
