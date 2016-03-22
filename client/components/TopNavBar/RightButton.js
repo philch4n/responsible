c4b91cdf72bb4533b318f8a7ede44bf6f50ae6a2
Grant Redfearn <grantredfearn@gmail.com>
2016-03-22 16:38:39 -0500

    (feat) quit driving and cancel ride button working for rider and driver

--- client/components/TopNavBar/RightButton.js
+++ client/components/TopNavBar/RightButton.js
@@ -8,7 +8,17 @@ export function TopNavBarRightButton({
   ride: { isMatched, isWaitingForMatch },
   ...onClicks,
 }) {
-  if ((isRider && isMatched) || (isDriver && isMatched)) {
+  if (isRider) {
+    return (
+    <div className="topNavBarRightButton">
+      {
+        isMatched || isWaitingForMatch ?
+          <CancelRideButton {...onClicks} /> :
+          <div />
+      }
+    </div>
+    );
+  } else if ((isDriver && isMatched) || (isRider && isMatched)) {
     return (
     <div className="topNavBarRightButton">
       {
@@ -31,3 +41,28 @@ export function TopNavBarRightButton({
   }
 }
 
+// import { CancelRideButton } from './CancelRideButton';
+
+// // export function TopNavBarRightButton({ isRider, isDriver, isMatched,
+// //   isWaitingForMatch, onProfileButtonClick, onCancelRideButtonClick, }) {
+// export function TopNavBarRightButton({
+//   user: { isRider, isDriver },
+//   ride: { isMatched, isWaitingForMatch },
+//   ...onClicks,
+// }) {
+//   if (isDriver || isRider) {
+//     return (
+//     <div className="topNavBarRightButton">
+//       {
+//         isMatched || isWaitingForMatch ?
+//           <CancelRideButton {...onClicks} /> :
+//           <div />
+//       }
+//     </div>
+//     );
+//   } else {
+//     return (
+//       <div />
+//     );
+//   }
+// }
