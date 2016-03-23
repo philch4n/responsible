de3bded25fe948f9b4246512ba9d136331a302b7
JosephPena <arcx354@gmail.com>
2016-03-23 18:58:09 -0500

    (feat) fully working Complete Ride Button.  Also capable of changing state

--- client/components/TopNavBar/CompleteRideButton.js
+++ client/components/TopNavBar/CompleteRideButton.js
@@ -1,6 +1,6 @@
-export function CompleteRideButton({ onCompleteRide }) {
+export function CompleteRideButton({ onComplete }) {
   return (
-    <div className='completeRideButton' onClick={onCompleteRide}>
+    <div className='completeRideButton' onClick={onComplete}>
       <a className='completeRideButton button is-danger is-large'>Ride Completed</a>
     </div>
   );
