e3a6c5c9cda1a0295b037c2667d9a0797f353838
Grant Redfearn <grantredfearn@gmail.com>
2016-03-24 14:47:26 -0500

    style-friends now round circles!

--- client/components/UserImage.js
+++ client/components/UserImage.js
@@ -1,11 +1,26 @@
+import { Grid, Row, Col, Image } from 'react-bootstrap';
+
 export function UserImage({ avatar, imageType }) {
   return (
     <div className="avatar">
     {
-      imageType === 'portrait' ?
-        <img className="userPortrait" src={ avatar /*  portrait size */ } /> :
-        <span><img className="userIcon" src={ avatar /*  icon size  */ } /></span>
+        <Grid>
+          <Row>
+            <Col xs={6} md={4}>
+              <Image className="userImage" src={ avatar } circle />
+            </Col>
+          </Row>
+        </Grid>
     }
     </div>
   );
 }
+
+// <span><img className="userIcon" src={ avatar /*  icon size  */ } /></span>
+//   <Grid>
+//     <Row>
+//       <Col xs={6} md={4}>
+//         <Image src={ avatar } circle />
+//       </Col>
+//     </Row>
+//   </Grid>
