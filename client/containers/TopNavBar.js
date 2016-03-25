9f0b196cb1941399d631a60f8773d44e18ddcbbf
Kimberly Kost <kek2009@gmail.com>
2016-03-25 15:19:41 -0500

    (feat) Logo redirects to main page

--- client/containers/TopNavBar.js
+++ client/containers/TopNavBar.js
@@ -25,7 +25,7 @@ function TopNavBar({ onCancel, onEndDriver, onPickUp, onComplete, onHomeClick, .
     <Navbar>
       <Navbar.Header className="mainHeader">
         <Navbar.Brand className="mainTitle">
-          <a href="#" bsClass="navHeader" >Fleet</a>
+          <a onClick={onHomeClick} bsClass="navHeader" >Fleet</a>
         </Navbar.Brand>
       </Navbar.Header>
       <Nav>
