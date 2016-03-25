1ad2bd1afa58966107372ee5b9072faedd523c86
JosephPena <arcx354@gmail.com>
2016-03-24 19:35:45 -0500

    (feat) fully working Facebook OAuth integeration

---
+++ client/models/Facebook.js
@@ -0,0 +1,58 @@
+var OAuth = require('../lib/oauth.min.js').OAuth;
+var OAuthUser = require('../lib/oauth.min.js').User;
+
+import { connect } from 'react-redux';
+import * as userAction from '../actionCreators/user';
+
+function nullFn(e) { console.log('you clicked me ' + e.target.className); };
+
+//OAuth Key Registered to Facebook, Github and Google
+OAuth.initialize('z7oz8f2CWDcLaaDjlXl4gH2NbHA');
+
+const User = module.exports;
+
+function Facebook({ onFacebookClick = nullFn, }) {
+  return (
+    <div className='FacebookButton' onClick={onFacebookClick}>
+      <a className='button'>
+      <i className='fa fa-facebook'></i>&nbsp;Facebook
+      </a>
+    </div>
+  );
+}
+
+const mapDispatchToProps = function (dispatch) {
+  return {
+    onFacebookClick() {
+      var user;
+      OAuth.popup('facebook', { cache: true })
+        .done(function (facebookToken) {
+          facebookToken.me()
+            .done(function (me) {
+              user = {
+                username: me.alias,
+                name: me.name,
+                avatar: me.avatar,
+                OAuthVerify: me.id,
+              };
+
+              var toSend = {
+                // verifyBy: 'OAuthVerify',
+                user: user,
+              };
+
+              dispatch(userAction.fetchUserInfo(toSend));
+            });
+        })
+        .fail(function (err) {
+          console.log('error', err);
+        });
+    },
+  };
+};
+
+export const FacebookButton = connect(
+  null,
+  mapDispatchToProps
+)(Facebook);
+
