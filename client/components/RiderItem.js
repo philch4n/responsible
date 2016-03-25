f9465e4dfdb844a6afc9c8f16f55311900319f11
Grant Redfearn <grantredfearn@gmail.com>
2016-03-25 15:00:43 -0500

    style-working on riderlist

--- client/components/RiderItem.js
+++ client/components/RiderItem.js
@@ -1,5 +1,5 @@
 import { UserImage } from './UserImage';
-
+import { Grid, Row, Col, Thumbnail } from 'react-bootstrap'
 export function RiderItem(props) {
 
   /* Commented this out to avoid lat/long errors */
@@ -11,10 +11,23 @@ export function RiderItem(props) {
   //     <div className='lng'>{location.lng}</div>
   //   </div>
   // );
+//   return (
+//     <div className="riderItem" onClick={props.onRiderItemClick}>
+//       <UserImage imageType="icon" avatar={props.avatar} />
+//       <div className="riderName">{props.name}</div>
+//     </div>
+//   );
+// // };
+
   return (
-    <div className="riderItem" onClick={props.onRiderItemClick}>
-      <UserImage imageType="icon" avatar={props.avatar} />
-      <div className="riderName">{props.name}</div>
-    </div>
+          <Col xs={6} md={4}>
+            <Thumbnail src={props.avatar} alt="242x200">
+              <h3>{props.name}</h3>
+              <p>{props.location}</p>
+              <p>
+                <Button bsStyle="default">Pick Up</Button>
+              </p>
+            </Thumbnail>
+          </Col>
   );
 };
