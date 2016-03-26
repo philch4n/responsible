a6fc3d45cf69e0ab24dcdabe0db8aab5e43a4e9e
Grant Redfearn <grantredfearn@gmail.com>
2016-03-26 16:35:17 -0500

    style-added riderItem div

--- client/components/RiderItem.js
+++ client/components/RiderItem.js
@@ -1,14 +1,18 @@
 import { UserImage } from './UserImage';
-import { Grid, Button, Row, Col, Thumbnail } from 'react-bootstrap';
+import { Grid, Button, Row, Col, Thumbnail } from 'react-bootstrap'
+
 export function RiderItem(props) {
+
   return (
-          <Col xs={6} md={4}>
-            <Thumbnail src={props.avatar} alt="121x100">
-              <h3>{props.name}</h3>
-              <p>
-                <Button bsStyle="primary" onClick={props.onRiderItemClick}>Pick Up</Button>
-              </p>
-            </Thumbnail>
-          </Col>
+    <Col xs={2} md={2} className="RiderItem">
+      <div className="riderItemDiv">
+          <UserImage imageType="riderImage" className="riderItemImage" avatar={ props.avatar } />
+          <h5 className="riderItemDesc">{props.name}</h5>
+          <p>
+            <Button bsStyle="primary" bsSize="small" className="riderItemButton" onClick={props.onRiderItemClick}>Pick Up</Button>
+          </p>
+      </div>
+    </Col>
   );
 };
+
